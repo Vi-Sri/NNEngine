63e93283a8d9d0c6c647a2e60ed1ad1f4087fbde
Srinivas Venkatanarayanan <srinivas.venka@hcl.com>
2019-11-26 15:45:59 +0530

    Added LayerProps

--- src/network.js
+++ src/network.js
@@ -2,21 +2,34 @@ import { InputLayer, Layer} from './layer';
 
 class Network {
     constructor(data) {
-        this.LayerList = new Array();
+        this.LayerList = {};
     }
 
     addLayer(layer) {
-        this.LayerList.push(layer);
-    }
+        LayerProps = {}
+        LayerProps['LayerName'] = layer.constructor.name;
+        LayerProps['LayerType'] = layer.constructor;
+        LayerProps['NeuronNumber'] = layer.neuronNumber;
+        LayerProps['ActivationType'] = layer.ActivationType;
+        LayerProps['LayerObject'] = layer;
+        this.LayerList.push(LayerProps);
+     }
 
-    train() {
-        return false;
+    forwardPropogate(dataset){
+        dataset.forEach(data => {
+            this.LayerList.forEach(layer => {
+                this.output = layer().calculatePredictions(data);
+            });
+        });
     }
 
     predict() {
-        this.LayerList.forEach(layer => {
-            layer
-        });
+        
+    
+    }
+
+    backwardPropogate() {
+
     }
 }
 
